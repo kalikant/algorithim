49d9203575d22ba1e95325a51b8e1776e592ea29
Kalikant <kalikant@users.noreply.github.com>
2017-11-05 20:04:01 +0530

    Update CamelCase.java

--- CamelCase.java
+++ CamelCase.java
@@ -11,33 +11,6 @@ All letters in the first word are lowercase.
 For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
 Given , print the number of words in  on a new line.
 
-Input Format
-
-A single line containing string .
-
-Constraints
-
-Output Format
-
-Print the number of words in string .
-
-Sample Input
-
-saveChangesInTheEditor
-Sample Output
-
-5
-Explanation
-
-String  contains five words:
-
-save
-Changes
-In
-The
-Editor
-Thus, we print  on a new line.
-
  */
 public class CamelCase {
 
