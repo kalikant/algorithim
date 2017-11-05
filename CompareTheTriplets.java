9af41f5530ff0560cb15dbba0dbe323f872fc1ec
Kalikant <kalikant@users.noreply.github.com>
2017-11-05 20:04:55 +0530

    Update CompareTheTriplets.java

--- CompareTheTriplets.java
+++ CompareTheTriplets.java
@@ -12,34 +12,6 @@ Comparison points is the total points a person earned.
 
 Given  and , can you compare the two challenges and print their respective comparison points?
 
-Input Format
-
-The first line contains  space-separated integers, , , and , describing the respective values in triplet . 
-The second line contains  space-separated integers, , , and , describing the respective values in triplet .
-
-Constraints
-
-Output Format
-
-Print two space-separated integers denoting the respective comparison points earned by Alice and Bob.
-
-Sample Input
-
-5 6 7
-3 6 10
-Sample Output
-
-1 1 
-Explanation
-
-In this example:
-
-Now, let's compare each individual score:
-
-, so Alice receives  point.
-, so nobody receives a point.
-, so Bob receives  point.
-Alice's comparison score is , and Bob's comparison score is . Thus, we print 1 1 (Alice's comparison score followed by Bob's comparison score) on a single line.
 */
 
 
