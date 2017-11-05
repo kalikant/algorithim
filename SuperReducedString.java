2319b6215959eadd7b26d3a940f359d1070b0425
Kalikant <kalikant@users.noreply.github.com>
2017-11-05 20:08:29 +0530

    Update SuperReducedString.java

--- SuperReducedString.java
+++ SuperReducedString.java
@@ -8,57 +8,6 @@ Steve wants to reduce  as much as possible. To do this, he will repeat the above
 
 Note: If the final string is empty, print Empty String .
 
-Input Format
-
-A single string, .
-
-Constraints
-
-Output Format
-
-If the final string is empty, print Empty String; otherwise, print the final non-reducible string.
-
-Sample Input 0
-
-aaabccddd
-Sample Output 0
-
-abd
-Sample Case 0
-
-Steve can perform the following sequence of operations to get the final string:
-
-aaabccddd → abccddd
-abccddd → abddd
-abddd → abd
-Thus, we print abd.
-
-Sample Input 1
-
-baab
-Sample Output 1
-
-Empty String
-Explanation 1
-
-Steve can perform the following sequence of operations to get the final string:
-
-baab → bb
-bb → Empty String
-Thus, we print Empty String.
-
-Sample Input 2
-
-aa
-Sample Output 2
-
-Empty String
-Explanation 2
-
-Steve can perform the following sequence of operations to get the final string:
-
-aa → Empty String
-Thus, we print Empty String.
 
  */
 public class SuperReducedString {
